a2609e12b4f48b48868da5cbc3dd5bc02632c631
Brennan Cheung <git@brennancheung.com>
2020-04-08 19:45:07 -0700

    Encode tests for each of the WASM module sections

--- src/wasm/__tests__/encode.test.js
+++ src/wasm/__tests__/encode.test.js
@@ -1,6 +1,9 @@
 import { adderBytes } from '../mocks/mocks'
-import { Op, ValType } from '../wasm'
+import { ExportDesc, Op, SectionId, ValType } from '../wasm'
 import {
+  encodeCodeSection,
+  encodeExportSection,
+  encodeFuncSection,
   encodeModule,
   encodeOp,
   encodeString,
@@ -10,6 +13,19 @@ import {
   sint,
   vector,
 } from '../encode'
+
+const addFunctionSpec = {
+  name: 'add',
+  params: [ValType.i32, ValType.i32],
+  results: [ValType.i32],
+  code: [
+    ...encodeOp(Op.localGet, 0),
+    ...encodeOp(Op.localGet, 1),
+    ...encodeOp(Op.i32Add),
+    ...encodeOp(Op.end),
+  ],
+  shouldExport: true
+}
 
 describe('ValType', () => {
   it('encodes ValType to binary', () => {
@@ -66,29 +82,17 @@ describe('module encoding', () => {
 
   it('encode adder WASM module', () => {
     const spec = {
-      functions: [
-        {
-          name: 'add',
-          params: [ValType.i32, ValType.i32],
-          results: [ValType.i32],
-          code: [
-            ...encodeOp(Op.localGet, 0),
-            ...encodeOp(Op.localGet, 1),
-            ...encodeOp(Op.i32Add),
-            ...encodeOp(Op.end),
-          ],
-          shouldExport: true
-        }
-      ]
+      functions: [addFunctionSpec]
     }
     const data = encodeModule(spec)
     expect(data).toEqual(adderBytes)
   })
 })
 
-describe('type section', () => {
-  it('encodes a simple type section', () => {
-
+// For binary encoding format see the spec at:
+// https://webassembly.github.io/spec/core/binary/modules.html
+describe('sections', () => {
+  it('encodes the "Type" section', () => {
     const data = encodeTypeSection([
       { params: [ValType.i32, ValType.i32], results: [ValType.i32] }
     ])
@@ -96,6 +100,45 @@ describe('type section', () => {
       0x01, 0x07, 0x01, 0x60, // type section
       0x02, 0x7F, 0x7F, // 2 params (i32, i32)
       0x01, 0x7F, // 1 result (i32)
+    ])
+  })
+
+  it('encodes the "Function" section', () => {
+    const fnTypes = [{ params: addFunctionSpec.params, results: addFunctionSpec.results }]
+    const fns = [addFunctionSpec]
+    expect(encodeFuncSection(fnTypes, fns)).toEqual([
+      SectionId.Function, // section id
+      2, // content size 2 bytes
+      1, // vector of size 1
+      0 // function 0 points to index 0
+    ])
+  })
+  
+  it('encodes the "Export" section', () => {
+    expect(encodeExportSection([addFunctionSpec])).toEqual([
+      SectionId.Export, // export section
+      7, // content size 7 bytes
+      1, // vector size 1
+      3, // string size
+      97, 100, 100, // "add"
+      ExportDesc.Func,
+      0, // function index 0
+    ])
+  })
+
+  it('encodes the "Code" section', () => {
+    expect(encodeCodeSection([addFunctionSpec])).toEqual([
+      SectionId.Code,
+      9, // content size 9 bytes
+      1, // vector size 1 code entry
+      7, // size of code entry 7 bytes
+      0, // vector size for number of locals
+      Op.localGet,
+      0, // $0
+      Op.localGet,
+      1, // $1
+      Op.i32Add,
+      Op.end,
     ])
   })
 })
